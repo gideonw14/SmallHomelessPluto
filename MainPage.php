7f8f2129f6b954ccf4f1942330e673a4fbab2cf6
Tim Regan <timregan10@gmail.com>
2017-05-03 19:04:43 -0500

    small changes

--- MainPage.php
+++ MainPage.php
@@ -5,17 +5,23 @@
 	require_once('sqlconnect.php');
 ?>
 
-<table width="1000">
+<table width="1000" border="1">
 <tbody>
 <tr>
 <td><b><span style="font-size:large;"> Solar System Database</span></b></td>
 <td align="right">
 
-<?php
-	echo "You are logged in as " . $_SESSION['user'] . 
-		"<td align=\"center\"><form action=\"logout.php\"><input type=\"submit\" value=\"Log Out\" /></form></td>";
-?>
 
+<?php
+	echo "You are logged in as " . $_SESSION['user'] . " and you have " . $_SESSION['accesslevel']
+			. " level access.";
+			
+	if ($_SESSION['accesslevel'] == "Admin") {
+		echo "<td align=\"center\"><form action=\"GrantAccess\"><input type=\"submit\" value=\"Grant Access\" />" .
+				"</form></td>";
+	}
+?>
+<td align="center"><form action="logout.php"><input type="submit" value="Log Out" /></form></td>
 
 </td>
 </tr>
@@ -25,7 +31,7 @@
 <p>Asteroids &amp; Meteors</p>
 <p>Stars</p>
 </td>
-<td>&nbsp;</td>
+<td colspan="4">&nbsp;</td>
 </tr>
 </tbody>
 </table>
