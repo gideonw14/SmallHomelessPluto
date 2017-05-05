24626a3e14796d65f6cfc26fac621d38483793f0
JKubs15 <jkubs15@gmail.com>
2017-05-04 21:04:19 -0500

    Commit create.php, add createselection.php

--- create.php
+++ create.php
@@ -26,38 +26,65 @@
 	<div>
 		Select what you would like to create:
 		
-		<form>
+		<form id="selection_form" action="createselection.php" method="get">
 			<select name="type_select" class="form-control">
-				<option value="Star">Star</option>
-				<option value="Planet">Planet</option>
-				<option value="Moon">Moon</option>
-				<option value="Star">Asteroid</option>
+                <option value=""></option>
+                <?php
+                    if($_SESSION['type_select'] == "Star") {
+                        echo '<option selected>Star</option>';
+                    } else {
+                        echo '<option value="Star">Star</option>';
+                    }
+                    if($_SESSION['type_select'] == "Planet") {
+                        echo '<option selected>Planet</option>';
+                    } else {
+                        echo '<option value="Planet">Planet</option>';
+                    }
+                    if($_SESSION['type_select'] == "Moon") {
+                        echo '<option selected>Moon</option>';
+                    } else {
+                        echo '<option value="Moon">Moon</option>';
+                    }
+                    if($_SESSION['type_select'] == "Asteroid") {
+                        echo '<option selected>Asteroid</option>';
+                    } else {
+                        echo '<option value="Asteroid">Asteroid</option>';
+                    }
+                ?>
 			</select>
+            <input type="submit" class="btn btn-default" value="Choose">
 		</form>
+
+        <?php
+            if($_SESSION['type_select'] == "Star") {
+                echo '<p>Star Surface Temperature: <input id="star_temp" name="star_temp" type="number" /></p>';
+           }
+        ?>
 		
-		<p>Star Surface Temperature: <input id="star_temp" name="star_temp" type="number" /></p>
-		
-		
-		Dwarf Planet: <form><input type="radio" name="dwarf" value="Yes" /> Yes<br />
-							  <input type="radio" name="dwarf" value="No" /> No</form>
-		
-		Population: <input type="number" name="population" /><br />
-		
-		Orbit Distance: <input type="number" name="orbit_dist" /><br />
-		
-		Year Length: <input type="number" name="year_length" /><br />
-		
-		Average Surface Temperature: <input type="number" name="surf_temp" /><br />
-		
-		In orbit around:
-		<?php 
-						$query = "SELECT SName FROM star";
-						$response = @mysqli_query($dbc, $query);
-						echo "<select name=\"star_select\" class=\"form-control\">";
-						while ($row = mysqli_fetch_array($response)) {
-							echo '<option value="' . $row['SName'] . '">' . $row['SName'] . '</option>';
-						}
-		?><br />
+		<?php
+            if($_SESSION['type_select'] == "Planet") {
+                echo 'Dwarf Planet: <form><input type="radio" name="dwarf" value="Yes" /> Yes<br />
+									  <input type="radio" name="dwarf" value="No" /> No</form>
+				
+				Population: <input type="number" name="population" /><br /><br />
+				
+				Orbit Distance: <input type="number" name="orbit_dist" /><br /><br />
+				
+				Year Length: <input type="number" name="year_length" /><br /><br />
+				
+				Average Surface Temperature: <input type="number" name="surf_temp" /><br /><br />
+				
+				In orbit around:';
+								$query = "SELECT SName FROM star";
+								$response = @mysqli_query($dbc, $query);
+								echo "<select name=\"star_select\" class=\"form-control\">";
+								while ($row = mysqli_fetch_array($response)) {
+									echo '<option value="' . $row['SName'] . '">' . $row['SName'] . '</option>';
+								}
+                                echo'</select>';
+            }
+        ?>
+        <br />
 	</div>
 	<form class="form-group" onsubmit="return false;">
 		<label for="name">Name</label>
